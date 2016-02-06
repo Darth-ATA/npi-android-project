68693f246c7f2c713650163d436935340068045c
Darth-ATA <hasbringer1007@gmail.com>
2016-02-06 21:54:33 +0100

    Finishes the GesturePhoto development, only fails when the user go back after take the photo it has to be fixed if the time permits it

--- NPIToolKit/app/src/main/java/com/alejandros/npitoolkit/GesturePhoto.java
+++ NPIToolKit/app/src/main/java/com/alejandros/npitoolkit/GesturePhoto.java
@@ -1,13 +1,21 @@
 package com.alejandros.npitoolkit;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class GesturePhoto extends AppCompatActivity {
+import haibison.android.lockpattern.LockPatternActivity;
+import haibison.android.lockpattern.utils.AlpSettings;
+
+public class GesturePhoto extends AppCompatActivity{
+
+    // App flags
+    private static final int REQ_CREATE_PATTERN = 1;
+    private static final int REQ_ENTER_PATTERN = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +25,73 @@ public class GesturePhoto extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // haibison.android.lockpattern.utils
+        // Save the pattern that the user introduces
+        AlpSettings.Security.setAutoSavePattern(this, true);
+
+        // Button that ables the user to create his own pattern
+        Button creationPatternButton = (Button) findViewById(R.id.createPattern);
+        creationPatternButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LockPatternActivity.IntentBuilder
+                        .newPatternCreator(GesturePhoto.this)
+                        .startForResult(GesturePhoto.this, REQ_CREATE_PATTERN);
+            }
+        });
+
+        // Button that waits to the previously defined pattern
+        Button insertPatternButton = (Button) findViewById(R.id.insertPattern);
+        insertPatternButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LockPatternActivity.IntentBuilder
+                        .newPatternComparator(GesturePhoto.this)
+                        .startForResult(GesturePhoto.this, REQ_ENTER_PATTERN);
+            }
+        });
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        switch(requestCode){
+            case REQ_CREATE_PATTERN: {
+                if (resultCode == RESULT_OK) {
+                    char[] pattern = data.getCharArrayExtra(LockPatternActivity.EXTRA_PATTERN);
+                    Toast.makeText(this, "Pattern:\n" + pattern, Toast.LENGTH_LONG).show();
+                }
+                break;
+            }// REQ_CREATE_PATTERN
+            case REQ_ENTER_PATTERN: {
+                // NOTE that there are 4 possible result codes!!!
+                switch(resultCode){
+                    case RESULT_OK:
+                        // The user passed
+                        Toast.makeText(this, "RESULT_OK", Toast.LENGTH_LONG).show();
+                        Intent intent = new Intent(this, CameraActivity.class);
+                        startActivity(intent);
+                        break;
+                    case RESULT_CANCELED:
+                        // The user cancelled the task
+                        Toast.makeText(this, "RESULT_CANCELED", Toast.LENGTH_LONG).show();
+                        break;
+                    case LockPatternActivity.RESULT_FAILED:
+                        // The user failed to enter the pattern
+                        Toast.makeText(this, "RESULT_FAILED", Toast.LENGTH_LONG).show();
+                        break;
+                    case LockPatternActivity.RESULT_FORGOT_PATTERN:
+                        // The user forgot the pattern and invoked your recovery Activity
+                        Toast.makeText(this, "RESULT_FORGOT_PATTERN", Toast.LENGTH_LONG).show();
+                        break;
+                }
+
+                // In any case, there's always a key EXTRA_RETRY_COUNT, which holds the number
+                // of tries that the user did
+                int retryCount = data.getIntExtra(LockPatternActivity.EXTRA_RETRY_COUNT, 0);
+
+                break;
+            }// REQ_ENTER_PATTERN
+        }
+    }
 }
--- README.md
+++ README.md
@@ -6,9 +6,20 @@ We are using Android Studio as IDE.
 ## QrGPSPoint
 For his developemt it decides to use a bridge withing the app and another app that scan the QR code. Using the classes **IntentIntegrator.java** and **IntentResult.java** provided by the library used **ZXing** permits the user scan the desired QR with an external application that he has installed before or the app will claim him to install it.
 
+## GesturePhoto
+For his development it decides to use as gesture all the combinations that provides de lockpatern of *Android*. For the lockpattern we use the library of [haibison](https://bitbucket.org/haibison/android-lockpattern/wiki/Quick-Use). After entering the correct lockpattern the app automatically opens a camera preview implemented using the [android development camera tutorial](http://stackoverflow.com/questions/2543059/android-camera-in-portrait-on-surfaceview) unfortunately the tutorial uses a deprecated class.
+
+After three seconds of entering the right pattern, it takes a photo and storaged in the external storage of the phone and the app shown a little message indicating where the photo has been placed.
+
 ## References
 ### Get Started
-http://developer.android.com/training/index.html
+[Tutorial](http://developer.android.com/training/index.html)
+### Common resources
+[Toolbar](http://developer.android.com/intl/es/reference/android/widget/Toolbar.html)
+[Toast](http://developer.android.com/intl/es/guide/topics/ui/notifiers/toasts.html)
+[Intent](http://developer.android.com/intl/es/reference/android/content/Intent.html)
+[Log](http://developer.android.com/intl/es/reference/android/util/Log.html)
+[Context](http://developer.android.com/intl/es/reference/android/content/Context.html)
 
 ### Qr
 
@@ -34,8 +45,24 @@ http://developer.android.com/training/index.html
 - http://stackoverflow.com/questions/10407159/how-to-manage-startactivityforresult-on-android/10407371#10407371
 
 ### GesturePhoto
+
+[Handler](http://developer.android.com/intl/es/reference/android/os/Handler.html)
+[Camera](http://developer.android.com/intl/es/guide/topics/media/camera.html#manifest)
+[FrameLayout](http://developer.android.com/intl/es/reference/android/widget/FrameLayout.html)
+[File](http://developer.android.com/intl/es/reference/java/io/File.html)
+[FileOutputStream](http://developer.android.com/intl/es/reference/java/io/FileOutputStream.html)
+[Uri](http://developer.android.com/intl/es/reference/android/net/Uri.html)
+[SurfaceView](http://developer.android.com/intl/es/reference/android/view/SurfaceView.html)
+[SurfaceHolder](http://developer.android.com/intl/es/reference/android/view/SurfaceHolder.html)
+
 https://bitbucket.org/haibison/android-lockpattern/wiki/Quick-Use
 http://stackoverflow.com/questions/3687315/deleting-shared-preferences
 http://stackoverflow.com/questions/8034127/how-to-remove-some-key-value-pair-from-sharedpreferences
 http://developer.android.com/intl/es/training/gestures/index.html
 http://developer.android.com/intl/es/training/gestures/detector.html
+
+http://developer.android.com/intl/es/guide/topics/media/camera.html#custom-camera
+http://stackoverflow.com/questions/9156928/can-error-in-opening-a-camera-be-fixed-in-android
+http://stackoverflow.com/questions/1520887/how-to-pause-sleep-thread-or-process-in-android
+http://stackoverflow.com/questions/2543059/android-camera-in-portrait-on-surfaceview
+http://stackoverflow.com/questions/20064793/how-to-fix-camera-orientation

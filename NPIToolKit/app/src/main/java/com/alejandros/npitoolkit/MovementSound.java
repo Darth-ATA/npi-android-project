63aace0db62847c98d18b7ac03435df0fd06c7a0
Alejandro Garcia Montoro <alejandro.garciamontoro@gmail.com>
2016-02-08 21:43:27 +0100

    Dunno if this works cause my phone is hell

--- NPIToolKit/app/src/main/java/com/alejandros/npitoolkit/MovementSound.java
+++ NPIToolKit/app/src/main/java/com/alejandros/npitoolkit/MovementSound.java
@@ -1,22 +1,56 @@
 package com.alejandros.npitoolkit;
-
+import android.hardware.SensorManager;
+import android.media.AudioAttributes;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.SoundPool;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+
+import com.alejandros.npitoolkit.AcelerometroData;
 
 public class MovementSound extends AppCompatActivity {
+    private AcelerometroData acelerometro;
+
+    SoundPool sp;
+    int soundId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movement_sound);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // Instaciamos la clase AcelerometroData que se encargará de tomar el dato del sensor
+        acelerometro = new AcelerometroData((SensorManager) getSystemService(SENSOR_SERVICE), this);
+
+        // http://stackoverflow.com/a/17070454/3248221
+        SoundPool sp = new SoundPool(5, AudioManager.STREAM_MUSIC, 0);
+
+        /** soundId for Later handling of sound pool **/
+        soundId = sp.load(this, R.raw.light_saber_on, 1); // in 2nd param u have to pass your desire ringtone
+    }
+
+    protected void onResume() {
+        super.onResume();
+        acelerometro.onResume();
+    }
+
+    protected void onPause() {
+        super.onPause();
+        acelerometro.onPause();
+    }
+
+    // Reproduce el sonido y realiza la animación de agitar el icono del móvil
+    // Se llama desde AcelerometroData cuando la aceleración lineal
+    // en el eje X supera un mínimo.
+    public void reproducirSonidoYAnimacion(){
+        sp.play(soundId, 1, 1, 0, 0, 1);
     }
 
 }
